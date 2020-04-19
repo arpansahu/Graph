a3c3207f9c19c0c64298c8070207ea1486b3124b
arpan sahu <arpanrocks95@gmail.com>
2020-04-19 22:17:20 +0530

    1
    1

---
+++ Implement_Graph_Data_Structure_in_C_Directed_Graph.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 6
+
+struct Node{
+    int destination;
+    struct Node * next;
+};
+
+struct Edge{
+    int source,destination;
+};
+
+struct Graph{
+    struct Node * head [N];
+};
+
+struct Graph * createGraph(struct Edge  edges[], int noOfEdges) {
+    struct Graph * graph = (struct Graph *)malloc(sizeof(struct Graph));
+    for (int i = 0; i < N; ++i) graph->head[i] = NULL;
+    for (int j = 0; j < noOfEdges; ++j) {
+        struct Node * newNode = (struct Node  *)malloc(sizeof(struct Node));
+        newNode->destination = edges[j].destination;
+        newNode->next = graph->head[edges[j].source];
+        graph->head[edges[j].source] = newNode;
+    }
+    return graph;
+}
+
+void printGraph(struct Graph * graph){
+    for (int i = 0; i < N; ++i) {
+        struct Node * temp = graph->head[i];
+        while(temp){ printf("(%d -> %d)",i,temp->destination);temp = temp->next;}printf("\n");
+    }
+}
+int main(void)
+{
+    struct Edge edges[] =
+            {
+                    { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
+                    { 3, 2 }, { 4, 5 }, { 5, 4 }
+            };
+
+    int  noOfEdges = sizeof(edges)/sizeof(edges[0]);
+    struct Graph *graph = createGraph(edges, noOfEdges);
+    printGraph(graph);
+    return 0;
+}
