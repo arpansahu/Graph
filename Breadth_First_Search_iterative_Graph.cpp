b65c81f0b5ee93765017c6e3fbe47559fbf478df
arpan sahu <arpanrocks95@gmail.com>
2020-04-21 22:19:14 +0530

    1
    1

--- Breadth_First_Search_iterative_Graph.cpp
+++ Breadth_First_Search_iterative_Graph.cpp
@@ -9,62 +9,97 @@ struct Edge {
 };
 
 class Graph{
-public:
+    int V;
     vector<vector<int>> adjList;
-    Graph(vector<Edge> edges,int N){
-        adjList.resize(N);
+public:
+
+    Graph(vector<Edge> edges,int V){
+        this->V = V;
+        adjList.resize(V);
         for(auto i : edges){
             adjList[i.source].push_back(i.destination);
-            adjList[i.destination].push_back(i.source);
+            //     adjList[i.destination].push_back(i.source);
         }
     }
+    void  BFSIterativelyJointGraph(int s);
+    void  BFSIterativelydisJointGraph();
+    void printGraph();
+    void BFSutility(vector <bool> &discovered ,int  i);
 };
 
-void printGraph(Graph graph)
+void Graph :: printGraph()
 {
-    for (int i = 0; i < graph.adjList.size(); i++)
+    for (int i = 0; i < this->adjList.size(); i++)
     {
         cout << i << " -- ";
-        for (int v : graph.adjList[i])
+        for (int v : this->adjList[i])
             cout <<"->"<< v << " ";
         cout << endl;
     }
 }
 
-void BFSIteratively(Graph  graph,vector<bool> &discovered){
-    for (int i = 0; i < graph.adjList.size() ; ++i) {
+void Graph :: BFSIterativelyJointGraph(int s){
+    vector<bool> discovered(V, false);
+    for(int i = 0; i < V; i++)
+        discovered[i] = false;
 
-        queue<int> q;
-        discovered[i] = true;
-        q.push(i);
+    queue <int> q;
+    discovered[s] = true;
+    q.push(s);
 
-        while(!q.empty()){
-            i = q.front();      q.pop();
-            cout << i << " ";
-            for (int j : graph.adjList[i]) {
-                if(!discovered[j]){
-                    discovered[j] = true;
-                    q.push(j);
-                }
+    while(!q.empty())
+    {
+        s = q.front();  cout << s << " ";   q.pop();
+        for (auto  j : this->adjList[s]) {
+            if(!discovered[j]){
+                discovered[j] = true;
+                q.push(j);
             }
         }
     }
 }
 
+void Graph :: BFSutility(vector <bool> &discovered ,int  s){
+    queue <int> q;
+    q.push(s);
+    discovered[s] = true;
+    while(!q.empty())
+    {
+        s = q.front();  cout << s << " ";   q.pop();
+
+        for (auto  j : this->adjList[s]) {
+            if(!discovered[j]){
+                discovered[j] = true;
+                q.push(j);
+            }
+        }
+    }
+}
+
+void Graph :: BFSIterativelydisJointGraph(){
+    vector <bool> discovered(V, false);
+    for(int i = 0; i <  V ; i++)   if(discovered[i] == false) BFSutility(discovered,i);
+}
 
 int main()
 {
 
     vector<Edge> edges =
             {
-                    {1, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {5, 9},
-                    {5, 10}, {4, 7}, {4, 8}, {7, 11}, {7, 12}
+                    {0, 1}, {0, 2}, {1, 2}, {2, 0}, {2,3},{3,3}
             };
 
-    int N = 15;
-    Graph graph(edges, N);
-    //   printGraph(graph);
-    vector<bool> discovered(N, false);
-    BFSIteratively(graph,discovered);
+    int V = 4;
+    Graph graph(edges, V);
+    //  graph.printGraph();
+    graph.BFSIterativelyJointGraph(2);
+    cout << "\n";
+    edges = {
+            {0,4},{1,2},{1,3},{1,4},{2,3},{3,4}
+    };
+
+    Graph graph2(edges,5);
+
+    graph2.BFSIterativelydisJointGraph();
     return 0;
 }
--- Breadth_First_Search_recursive_Graph.cpp
+++ Breadth_First_Search_recursive_Graph.cpp
@@ -2,6 +2,7 @@
 #include "iostream"
 #include "vector"
 #include "queue"
+
 using namespace std;
 
 struct Edge {
@@ -9,36 +10,44 @@ struct Edge {
 };
 
 class Graph{
-public:
+    int V;
     vector<vector<int>> adjList;
-    Graph(vector<Edge> edges,int N){
-        adjList.resize(N);
+public:
+
+    Graph(vector<Edge> edges,int V){
+        this->V = V;
+        adjList.resize(V);
         for(auto i : edges){
             adjList[i.source].push_back(i.destination);
-            adjList[i.destination].push_back(i.source);
+            //     adjList[i.destination].push_back(i.source);
         }
     }
+    void BFSRecursivelyJoinandDisjointtGraphUtil(vector<bool> &discovered, queue<int> &q);
+    void BFSRecursivelyJointandDisjointGraph(int s);
+    void printGraph();
+
+
 };
 
-void printGraph(Graph graph)
+void Graph :: printGraph()
 {
-    for (int i = 0; i < graph.adjList.size(); i++)
+    for (int i = 0; i < this->adjList.size(); i++)
     {
         cout << i << " -- ";
-        for (int v : graph.adjList[i])
+        for (int v : this->adjList[i])
             cout <<"->"<< v << " ";
         cout << endl;
     }
 }
 
-void BFSRecursively(Graph  graph,vector<bool> &discovered,queue<int> & q){
+
+void Graph ::BFSRecursivelyJoinandDisjointtGraphUtil(vector<bool> &discovered, queue<int> &q) {
     if (q.empty())
         return;
     int v = q.front();
     q.pop();
-    cout << v << " ";
-
-    for (int u : graph.adjList[v])
+    cout << v <<" ";
+    for (int u : this->adjList[v])
     {
         if (!discovered[u])
         {
@@ -46,30 +55,47 @@ void BFSRecursively(Graph  graph,vector<bool> &discovered,queue<int> & q){
             q.push(u);
         }
     }
+    BFSRecursivelyJoinandDisjointtGraphUtil(discovered, q);
 
-    BFSRecursively(graph, discovered,q);
 }
 
+void Graph ::BFSRecursivelyJointandDisjointGraph(int s) {
+    vector<bool> discovered(V, false);
+    queue<int> q;
+
+    for (int i = s; i < V; i++) {
+        if (discovered[i] == false)
+        {
+            discovered[i] = true;
+            q.push(i);
+            BFSRecursivelyJoinandDisjointtGraphUtil(discovered, q);
+        }
+    }
+}
 
 int main()
 {
 
     vector<Edge> edges =
             {
-                    {1, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {5, 9},
-                    {5, 10}, {4, 7}, {4, 8}, {7, 11}, {7, 12}
+                    {0, 1}, {0, 2}, {1, 2}, {2, 0}, {2,3},{3,3}
             };
 
-    int N = 15;
-    Graph graph(edges, N);
- //   printGraph(graph);
-    vector<bool> discovered(N, false);
-    queue<int> q;
-    for (int i = 0; i < N; ++i) {
-        discovered[i] = true;
-        q.push(i);
-        BFSRecursively(graph,discovered,q);
-    }
+    int V = 4;
+    Graph graph(edges, V);
+    //   graph.printGraph();
+    graph.BFSRecursivelyJointandDisjointGraph(2);
+    cout << "\n";
 
+
+
+
+    edges = {
+            {0,4},{1,2},{1,3},{1,4},{2,3},{3,4}
+    };
+
+    Graph graph2(edges,5);
+
+    graph2.BFSRecursivelyJointandDisjointGraph(0);
     return 0;
 }
