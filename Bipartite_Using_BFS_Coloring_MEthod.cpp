ba8b0fbdb707ddce235e17813c28611e00a245e2
arpan sahu <arpanrocks95@gmail.com>
2020-04-22 22:24:09 +0530

    1
    1

--- Bipartite_Using_BFS_Coloring_MEthod.cpp
+++ Bipartite_Using_BFS_Coloring_MEthod.cpp
@@ -1,7 +1,7 @@
 #include <queue>
 #include "iostream"
 #include "vector"
-#include "queue"
+#include "set"
 using namespace std;
 
 struct Edge {
@@ -18,20 +18,20 @@ public:
         adjList.resize(V);
         for(auto i : edges){
             adjList[i.source].push_back(i.destination);
-            adjList[i.destination].push_back(i.source);
+            //  adjList[i.destination].push_back(i.source);
         }
     }
     void  Bipartite();
     void printGraph();
-    void Bipartiteutility(vector <char> &discovered ,int  i);
+    void Bipartiteutility(vector<char> &discovered, int i, bool &cond);
 };
 
 void Graph :: printGraph()
 {
-    for (int i = 0; i < this->adjList.size(); i++)
+    for (int i = 0; i < adjList.size(); i++)
     {
         cout << i << " -- ";
-        for (int v : this->adjList[i])
+        for (int v : adjList[i])
             cout <<"->"<< v << " ";
         cout << endl;
     }
@@ -39,27 +39,29 @@ void Graph :: printGraph()
 }
 
 
-void Graph :: Bipartiteutility(vector <char> &colored ,int  s){
+void Graph ::Bipartiteutility(vector<char> &discovered, int i, bool &cond) {
     queue <int> q;
-    q.push(s);
-    colored[s] = 'R';
+    q.push(i);
+    discovered[i] = 'R';
     while(!q.empty())
     {
-        s = q.front();    q.pop();  //cout << s<<" " ;
-        for (auto  j : this->adjList[s]) {
-            if(colored[j] == NULL){
-                colored[j] = (colored[s] == 'R') ? 'B' : 'R';
+        i = q.front();    q.pop();  //cout << s<<" " ;
+        for (auto  j : adjList[i]) {
+            if(discovered[j] == NULL){
+                discovered[j] = (discovered[i] == 'R') ? 'B' : 'R';
                 q.push(j);
             }
-            else if (colored[s] == colored[j]){ cout << "Not a Bipartite Graph  "; return;}
+            else if (discovered[i] == discovered[j]){ cond = false ;return;}
         }
     }
-    cout <<"Bipartite Graph";
+
 }
 
 void Graph :: Bipartite(){
+    bool cond = true;
     vector <char> colored(V, NULL);
-    for(int i = 0; i <  V ; i++)   if(colored[i] == NULL) Bipartiteutility(colored,i);
+    for(int i = 0; i <  V ; i++)   if(colored[i] == NULL && cond == true) Bipartiteutility(colored, i, cond);
+    (cond == true) ? cout << "Bipartite Graph  \n" :cout <<"Not a Bipartite Graph  \n";
 }
 
 int main()
@@ -71,10 +73,23 @@ int main()
 
             };
 
-    int V = edges.size();
+    set <int > setsize;
+    for(auto  i : edges){ setsize.insert(i.source);setsize.insert(i.destination);}
+    int V = setsize.size();
     Graph graph(edges, V);
     //  graph.printGraph();
     graph.Bipartite();
 
+    vector<Edge> edges1 =
+            {
+                    {2,1},{0,3},{1,0},{4,2},{5,1},{2,0}
+            };
+
+    set <int > setsize1;
+    for(auto  i : edges1){ setsize1.insert(i.source);setsize1.insert(i.destination);}
+    int V1 = setsize1.size();
+    Graph graph1(edges1, V1);
+    //  graph.printGraph();
+    graph1.Bipartite();
     return 0;
 }
